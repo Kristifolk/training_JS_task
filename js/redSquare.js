eacafff3161c0eb8cd79e31e8ef1a7fccbe2b376
kristin <devkristin.karr@gmail.com>
2024-08-22 17:56:07 +0300

    edited moveRight()

--- js/redSquare.js
+++ js/redSquare.js
@@ -1,21 +1,10 @@
-// const square =document.getElementsByClassName('square');
 const square =document.getElementById('movingSquare');
-let startX = 10;
-let startY = 0
+let startX = 0;
+let timer = setInterval(()=>moveRight(),500 );
 
-let timer = setInterval(()=>move(),500 );
-// setTimeout(()=>{
-//     clearInterval(timer); функция, время
-//
-// })
-// function move() {
-//     square.classList.toggle('hidden')
-//     square.classList.toggle('hidden')
-// }
-
-function move() {
-    startX +=10;
-
+function moveRight() {
+    startX +=1;
+    square.style.left = startX + 'px';
 }
 
-
+setTimeout(() => clearInterval(timer), 60000);//стоп после 60 сек
